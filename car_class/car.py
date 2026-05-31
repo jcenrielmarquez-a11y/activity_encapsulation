965491b5f795a2e4f11a3b4a5b16016cccd7e0f4
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-05-31 20:15:23 +0800

    I added accelerate and brake methods to control the Car speed and a getter to check the current speed.

--- car_class/car.py
+++ car_class/car.py
@@ -3,3 +3,15 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+
+    def get_speed(self):
+        return self.__speed
