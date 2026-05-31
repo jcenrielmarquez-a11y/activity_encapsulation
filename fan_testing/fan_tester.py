6edfbe1afa05d5cab0afbad7f9001cc160b14120
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-05-31 19:13:19 +0800

    I added print statements in the runner to show fan2 details like speed, radius, color, and on i also add space in the #display fan1 details

--- fan_testing/fan_tester.py
+++ fan_testing/fan_tester.py
@@ -4,8 +4,14 @@ def main():
     fan1 = Fan(Fan.FAST, 10, "yellow", True)
     fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
-    #display fan1 details
+    # display fan1 details
     print("Fan1_Speed:", fan1.get_speed(),
           "Radius:", fan1.get_radius(),
           "Color:", fan1.get_color(),
           "On:", fan1.is_on())
+
+    # display fan2 details
+    print("Fan2 -> Speed:", fan2.get_speed(),
+          "Radius:", fan2.get_radius(),
+          "Color:", fan2.get_color(),
+          "On:", fan2.is_on())
