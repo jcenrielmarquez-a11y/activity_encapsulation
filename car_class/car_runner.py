caf697ea60047c4c7c12fd314320d6cd08d40aa3
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-06-01 14:35:53 +0800

    I added a print statement in main() to show the car’s year model and make using the getter methods.

--- car_class/car_runner.py
+++ car_class/car_runner.py
@@ -3,4 +3,7 @@ from car import Car
 def main():
     year_model = int(input("Enter a Year Model of Car: "))
     make = input("Enter a model of Car: ")
+
     car = Car(year_model, make)
+
+    print(f"\nYear Model: {car.get_make()}, Make: {car.get_year_model()}\n")
