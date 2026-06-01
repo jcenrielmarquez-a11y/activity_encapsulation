ba205c56e18b11e05979955b71a46f2e91bd6497
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-06-01 16:04:52 +0800

    Added setters for Pet attributes with age check that the age must be non-negative.

--- pet_profile/pet_record.py
+++ pet_profile/pet_record.py
@@ -3,3 +3,16 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+
+    #setters
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        if age >= 0:
+            self.__age = age
+        else:
+            print("Invalid age. Must be non-negative.")
