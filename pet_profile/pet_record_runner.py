bfd9c56836e50b4f3edc4c062e37862211bb9162
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-06-01 16:12:33 +0800

    I added a main function that asks the user for pet details (name, type, and age).

---
+++ pet_profile/pet_record_runner.py
@@ -0,0 +1,6 @@
+from pet_record import Pet
+
+def main():
+    name = input("Enter your pet's name: ")
+    animal_type = input("Enter your pet's type: ")
+    age = int(input("Enter your pet's age: "))
