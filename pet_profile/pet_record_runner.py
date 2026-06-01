64e71877ce7587a149010acc6c1e81a1939a7d57
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-06-01 16:15:24 +0800

    I added print statements in main() to show the Pet’s name, type, and age using the getter methods.

--- pet_profile/pet_record_runner.py
+++ pet_profile/pet_record_runner.py
@@ -9,3 +9,8 @@ def main():
     pet.set_name(name)
     pet.set_animal_type(animal_type)
     pet.set_age(age)
+
+    print("\nPet Details:")
+    print("Name:", pet.get_name())
+    print("Type:", pet.get_animal_type())
+    print("Age:", pet.get_age())
